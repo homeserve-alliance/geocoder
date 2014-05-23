47fbb3d91afa3c947fcaf8c62b5fcf1f55201b9e
Rob Murray <robmurray17@gmail.com>
2014-05-23 13:34:34 +0100

    Specify PostcodeAnywhere fixture prefix

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -196,6 +196,17 @@ module Geocoder
         "#{fixture_prefix}_romsey"
       end
     end
+
+    class PostcodeAnywhereAddress
+      private
+      def fixture_prefix
+        'postcode_anywhere_address_v1_x'
+      end
+
+      def default_fixture_filename
+        "#{fixture_prefix}_N19AE"
+      end
+    end
   end
 end
 
